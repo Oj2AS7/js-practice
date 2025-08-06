29e62a9cafef3c61500dadee8822208e754c6bb2
OJAS <ojas.271001@gmail.com>
2025-08-06 17:50:40 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,38 @@
+// let score = 33;
+// let score = "33"
+// let score = "33abc"
+// let score = null;
+// let score = undefined;
+// let score = true;
+let score = "ojas";
+console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+console.log(typeof(ValueInNumber));
+console.log(ValueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+// to convert the value in boolean
+
+
+//let isloggedin = 1;
+// let isloggedin = "";
+// let isloggedin = "ojas";
+// let isloggedin = 123;
+let isloggedin = 0;
+let booleanisloggedin = Boolean(isloggedin);
+console.log(booleanisloggedin);
+
+
+// 1 to infinity, "anything" => true
+// 0 , "" => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
