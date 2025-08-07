92aaf8f75e47b53cb940d536f3d2a3b1ad5710b5
OJAS <ojas.271001@gmail.com>
2025-08-07 07:07:31 +0000

    Operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,38 +1,91 @@
-// let score = 33;
-// let score = "33"
-// let score = "33abc"
-// let score = null;
-// let score = undefined;
-// let score = true;
-let score = "ojas";
-console.log(typeof(score));
+// // let score = 33;
+// // let score = "33"
+// // let score = "33abc"
+// // let score = null;
+// // let score = undefined;
+// // let score = true;
+// let score = "ojas";
+// console.log(typeof(score));
 
-let ValueInNumber = Number(score)
-console.log(typeof(ValueInNumber));
-console.log(ValueInNumber);
-
-
-// "33" => 33
-// "33abc" => NaN
-// true => 1, false => 0
-
-// to convert the value in boolean
+// let ValueInNumber = Number(score)
+// console.log(typeof(ValueInNumber));
+// console.log(ValueInNumber);
 
 
-//let isloggedin = 1;
-// let isloggedin = "";
-// let isloggedin = "ojas";
-// let isloggedin = 123;
-let isloggedin = 0;
-let booleanisloggedin = Boolean(isloggedin);
-console.log(booleanisloggedin);
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1, false => 0
+
+// // to convert the value in boolean
 
 
-// 1 to infinity, "anything" => true
-// 0 , "" => false
+// //let isloggedin = 1;
+// // let isloggedin = "";
+// // let isloggedin = "ojas";
+// // let isloggedin = 123;
+// let isloggedin = 0;
+// let booleanisloggedin = Boolean(isloggedin);
+// console.log(booleanisloggedin);
 
-let someNumber = 33
 
-let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// // 1 to infinity, "anything" => true
+// // 0 , "" => false
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// // console.log(stringNumber);
+// // console.log(typeof stringNumber);
+
+// ****************OPERATIONS********************
+
+// let value = 3;
+// let negvalue = -value;
+// console.log(negvalue);
+// console.log(2**5);
+// console.log(2%5);
+
+// let str1 = "ojas";
+// let str2 = " kotiyal";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// //surprise!!!
+// console.log("1"+2);
+// console.log("1"+2+2); //again surprise
+// console.log(2+3+"1"); //again surprise
+// console.log(+true);
+// console.log(+"");
+
+// let gamecounter = 100;
+// // gamecounter++
+// ++gamecounter
+// console.log(gamecounter);
+
+
+// // postfix increment - If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+// console.log(x);
+// console.log(y);
+
+// let x2 = 3n;
+// const y2 = x2++;
+// // x2 is 4n; y2 is 3n
+// console.log(x2);
+// console.log(y2);
+
+
+// prefix increment - If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+console.log(x);
+console.log(y)
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+console.log(x2);
+console.log(y2);
