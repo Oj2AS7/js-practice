e9051518331789cbed5a0ba6bc9405dc6cfef8bb
OJAS <ojas.271001@gmail.com>
2025-08-07 08:17:54 +0000

    premetive and non-premetives

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+// Premitive datatypes (call by value)
+
+// string,number,boolean,null,undefined,symbol,bigint
+
+const id = Symbol('123');
+const anotherid = Symbol('123');
+console.log(id===anotherid);
+
+// Non-Premitive dataypes (Refrence type) (call by refrence)
+
+// arrays,objects,functions
+
+
+
+const crushes = ["batool","manya","palak pawaar"]
+
+let myobj = {
+    name: ojas,
+    age: 18,
+}
+
+const myfunc = function(){
+    console.log("hello world");
+}
+
+
+console.log(typeof anotherId); // do typeof of all the variables
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
