f056dcef7f76fd0fa598a73a394ef8569e9452fa
OJAS <ojas.271001@gmail.com>
2025-08-07 07:20:09 +0000

    to stydy comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log("2">1);  //true
+// console.log("A">1);  //false
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+                            //avoid all these kind of coversions
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<0);
+
+// (===) -> this is use for strict check which also checks the datatype
+console.log("2"===2);
